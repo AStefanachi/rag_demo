a270ce78431664b9b9c6be6b8014927caf15fbb7
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 05:02:30 +0100

    aider: Formatted `utils\constants.py` according to PEP 8 standards by adding a module docstring, improving comments, and separating import statements.
    # Aider chat conversation:
    
    ASSISTANT: To properly comment and format `utils\constants.py` according to PEP 8 standards, we will add a module docstring, improve the comments for clarity, and ensure that the import statements are grouped and separated according to PEP 8 guidelines.
    
    Here is the *SEARCH/REPLACE* block for `utils\constants.py`:
    
    utils\constants.py
    ```python
    <<<<<<< SEARCH
    # utils\constants.py
    import os, sys
    
    HOME_DIR = sys.path[0]
    SAMPLE_SOURCES_PDF = os.path.join(HOME_DIR, "sample_sources", "pdf")
    CHROMA_PERSISTANT_DIR = os.path.join(HOME_DIR, "chromadb")
    PROMPT_YAML_DIR = os.path.join(HOME_DIR, "prompt_yaml")
    =======
    """
    Constants used throughout the utils package.
    
    This module defines path constants that are used across different modules
    within the utils package for file and directory management.
    """
    
    import os
    import sys
    
    # Define the home directory based on the system path
    HOME_DIR = sys.path[0]
    
    # Define the path to the sample sources PDF directory
    SAMPLE_SOURCES_PDF = os.path.join(HOME_DIR, "sample_sources", "pdf")
    
    # Define the path to the ChromaDB persistent directory
    CHROMA_PERSISTANT_DIR = os.path.join(HOME_DIR, "chromadb")
    
    # Define the path to the directory containing prompt YAML files
    PROMPT_YAML_DIR = os.path.join(HOME_DIR, "prompt_yaml")
    >>>>>>> REPLACE
    ```
    
    This change adds a more descriptive module docstring, separates the import statements into different lines, and adds comments that describe the purpose of each constant.

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,22 @@
 # utils\constants.py
-import os, sys
+"""
+Constants used throughout the utils package.
 
+This module defines path constants that are used across different modules
+within the utils package for file and directory management.
+"""
+
+import os
+import sys
+
+# Define the home directory based on the system path
 HOME_DIR = sys.path[0]
+
+# Define the path to the sample sources PDF directory
 SAMPLE_SOURCES_PDF = os.path.join(HOME_DIR, "sample_sources", "pdf")
+
+# Define the path to the ChromaDB persistent directory
 CHROMA_PERSISTANT_DIR = os.path.join(HOME_DIR, "chromadb")
+
+# Define the path to the directory containing prompt YAML files
 PROMPT_YAML_DIR = os.path.join(HOME_DIR, "prompt_yaml")
