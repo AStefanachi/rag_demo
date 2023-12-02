10fa5259f99a605c2eb6a2000c3f8d1e1b90892a
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 05:26:15 +0100

    aider: Updated `static\script.js` to populate the dropdown menu with document names from `/sample_sources/pdf` directory and removed the collection name from the form submission.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: static\script.js
    
    ASSISTANT: Great, now that `static\script.js` is a *read-write* file, we can proceed with the changes. We need to update `static\script.js` to populate the dropdown menu with the document names from the `/sample_sources/pdf` directory and remove the collection name since it's now locked to `'LG_DRYER_DEMO'`.
    
    Here are the *SEARCH/REPLACE* blocks for the required changes:
    
    static\script.js
    ```javascript
    <<<<<<< SEARCH
    var collectionName = document.getElementById('collection-name').value;
    fetch('/load_vectorstore', {
        method: 'POST',
        body: new URLSearchParams({
            'document_name': documentName,
            'collection_name': collectionName
    =======
    fetch('/load_vectorstore', {
        method: 'POST',
        body: new URLSearchParams({
            'document_name': documentName
    >>>>>>> REPLACE
    ```
    
    Additionally, we need to add the code to populate the dropdown menu when the page is loaded. This requires adding a new block of code to `static\script.js`.
    
    static\script.js
    ```javascript
    <<<<<<< SEARCH
    document.addEventListener('DOMContentLoaded', function() {
    =======
    document.addEventListener('DOMContentLoaded', function() {
        // Fetch the list of documents and populate the dropdown
        fetch('/list_documents')
            .then(response => response.json())
            .then(files => {
                const selectElement = document.getElementById('document-name');
                files.forEach(file => {
                    const option = document.createElement('option');
                    option.value = option.textContent = file;
                    selectElement.appendChild(option);
                });
            });
    
    >>>>>>> REPLACE
    ```
    
    With these changes, `static\script.js` will now populate the dropdown menu with the document names from the `/sample_sources/pdf` directory when the page is loaded, and it will no longer send the `collection_name` when submitting the form to load a document, as the collection name is now fixed to `'LG_DRYER_DEMO'`.

--- static/script.js
+++ static/script.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Fetch the list of documents and populate the dropdown
+    fetch('/list_documents')
+        .then(response => response.json())
+        .then(files => {
+            const selectElement = document.getElementById('document-name');
+            files.forEach(file => {
+                const option = document.createElement('option');
+                option.value = option.textContent = file;
+                selectElement.appendChild(option);
+            });
+        });
     document.getElementById('load-form').addEventListener('submit', function(e) {
         e.preventDefault();
         var documentName = document.getElementById('document-name').value;
-        var collectionName = document.getElementById('collection-name').value;
         fetch('/load_vectorstore', {
             method: 'POST',
             body: new URLSearchParams({
-                'document_name': documentName,
-                'collection_name': collectionName
+                'document_name': documentName
             })
         })
         .then(response => response.json())
