c67f584a918c276a1cbfef18f4d880486270bb00
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 04:42:34 +0100

    aider: Formatted and commented `rag_demo\orchestration.py` for proper documentation and clarity.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: settings.env
    
    ASSISTANT: Great, now that `settings.env` is a *read-write* file, please let me know what changes you would like to make to it or any of the other *read-write* files you've provided.
    
    USER: Properly comment and format `rag_demo\orchestration.py`
    
    ASSISTANT: To properly comment and format `rag_demo\orchestration.py`, we will:
    
    1. Add a module docstring at the top of the file to describe the purpose of the module.
    2. Add a function docstring to `load_chromadb` to describe its behavior, parameters, and return value.
    3. Ensure consistent spacing and indentation throughout the file.
    4. Add inline comments where necessary to explain complex or non-obvious parts of the code.
    
    Here is the *SEARCH/REPLACE* block for `rag_demo\orchestration.py`:
    
    rag_demo\orchestration.py
    ```python
    <<<<<<< SEARCH
    # rag_demo\orchestration.py
    
    from utils.classes import Orchestrator
    from .load_pdf import load_pdfloader
    from .load_vectorstore import load_vectorstore_chromadb
    
    def load_chromadb(document_name: str, collection_name: str):
    
        orchestrator = Orchestrator()
        orchestrator.load_function(load_pdfloader, document_name)
        orchestrator.load_function(lambda result: load_vectorstore_chromadb(documents=result, collection_name=collection_name))
        # Execute the loaded functions sequentially and return the results
        results = orchestrator.run()
        return [output for output in results] if results else False
    =======
    """
    rag_demo.orchestration
    ~~~~~~~~~~~~~~~~~~~~~~
    
    This module provides orchestration for loading documents into a ChromaDB vector store.
    """
    
    from utils.classes import Orchestrator
    from .load_pdf import load_pdfloader
    from .load_vectorstore import load_vectorstore_chromadb
    
    def load_chromadb(document_name: str, collection_name: str):
        """
        Orchestrates the loading of a document into a ChromaDB vector store.
    
        Args:
            document_name (str): The name of the document to be loaded.
            collection_name (str): The name of the collection where the document will be stored.
    
        Returns:
            list: A list of outputs from the loaded functions or False if no results.
        """
        # Initialize the orchestrator
        orchestrator = Orchestrator()
        # Load the PDF loader function with the document name
        orchestrator.load_function(load_pdfloader, document_name)
        # Load the vector store function with the result of the PDF loader and collection name
        orchestrator.load_function(lambda result: load_vectorstore_chromadb(documents=result, collection_name=collection_name))
        # Execute the loaded functions sequentially and return the results
        results = orchestrator.run()
        # Return the results as a list if there are any, otherwise return False
        return [output for output in results] if results else False
    >>>>>>> REPLACE
    ```
    
    This block adds a module docstring, a function docstring for `load_chromadb`, and improves the inline comments for clarity. The code formatting, such as indentation and spacing, is already consistent with Python standards, so no changes are needed there.

--- rag_demo/orchestration.py
+++ rag_demo/orchestration.py
@@ -1,14 +1,33 @@
 # rag_demo\orchestration.py
+"""
+rag_demo.orchestration
+~~~~~~~~~~~~~~~~~~~~~~
+
+This module provides orchestration for loading documents into a ChromaDB vector store.
+"""
 
 from utils.classes import Orchestrator
 from .load_pdf import load_pdfloader
 from .load_vectorstore import load_vectorstore_chromadb
 
 def load_chromadb(document_name: str, collection_name: str):
+    """
+    Orchestrates the loading of a document into a ChromaDB vector store.
 
+    Args:
+        document_name (str): The name of the document to be loaded.
+        collection_name (str): The name of the collection where the document will be stored.
+
+    Returns:
+        list: A list of outputs from the loaded functions or False if no results.
+    """
+    # Initialize the orchestrator
     orchestrator = Orchestrator()
+    # Load the PDF loader function with the document name
     orchestrator.load_function(load_pdfloader, document_name)
+    # Load the vector store function with the result of the PDF loader and collection name
     orchestrator.load_function(lambda result: load_vectorstore_chromadb(documents=result, collection_name=collection_name))
     # Execute the loaded functions sequentially and return the results
     results = orchestrator.run()
+    # Return the results as a list if there are any, otherwise return False
     return [output for output in results] if results else False
