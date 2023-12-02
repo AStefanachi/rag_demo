2942db6b7796ddc0aa05c057726d3a13afe88ea3
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 03:57:16 +0100

    Refactor PyPDFLoader into a PDFLoader class.

--- utils/classes.py
+++ utils/classes.py
@@ -1,11 +1,6 @@
-# This is a working example of the PyPDF Loader
+# utils\classes.py
 
 from langchain.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader(r"C:\PRIVATE\rag_demo\sample_sources\pdf\DR_EUS_MFL70442678_00_221121_00_WEB_EN.pdf")
-pages = loader.load_and_split()
-
-# I want you to generalize it in a class
 
 class PDFLoader:
 
