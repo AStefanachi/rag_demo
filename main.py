b75544d68d874cf35b7c8ec9a4ea92c6f9dd7793
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 05:10:46 +0100

    aider: Refactored `main.py` to include a `main` function that uses `argparse` to handle two options (`load_vectorstore` and `query`) and calls the appropriate functions based on the chosen subcommand.
    # Aider chat conversation:
    
    ASSISTANT: To create a `main` function in `main.py` that uses `argparse` to handle two options (`load_vectorstore` and `query`), we need to:
    
    1. Import the `argparse` module.
    2. Define the `main` function.
    3. Set up an `ArgumentParser`.
    4. Add the `load_vectorstore` and `query` subparsers.
    5. Add the required arguments for each subparser based on the functions `load_chromadb` and `query_LG_DRYER_DEMO`.
    6. Implement the logic to call the appropriate function based on the chosen subcommand.
    7. Call the `main` function when the script is executed directly.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    main.py
    ```python
    <<<<<<< SEARCH
    =======
    import argparse
    from rag_demo.orchestration import load_chromadb
    from rag_demo.retrievals import query_LG_DRYER_DEMO
    
    def main():
        parser = argparse.ArgumentParser(description="RAG Demo Orchestration")
        subparsers = parser.add_subparsers(dest='command', required=True)
    
        # Subparser for load_vectorstore
        parser_load = subparsers.add_parser('load_vectorstore', help='Load a document into ChromaDB vector store')
        parser_load.add_argument('document_name', type=str, help='The name of the document to be loaded')
        parser_load.add_argument('collection_name', type=str, help='The name of the collection where the document will be stored')
    
        # Subparser for query
        parser_query = subparsers.add_parser('query', help='Query the LG_DRYER_DEMO collection from the vectorstore')
        parser_query.add_argument('prompt', type=str, help='The question to query the vectorstore with')
    
        args = parser.parse_args()
    
        if args.command == 'load_vectorstore':
            result = load_chromadb(args.document_name, args.collection_name)
            print(result)
        elif args.command == 'query':
            result = query_LG_DRYER_DEMO(args.prompt)
            print(result)
    
    if __name__ == '__main__':
        main()
    >>>>>>> REPLACE
    ```
    
    This block of code will replace the entire content of `main.py` with the new `main` function and the necessary imports and argument parsing logic. Please confirm if you would like to proceed with this change or if you have any modifications or additional instructions.

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 # I want you to create a main function
 # The main function will have two options that should be used via argparse: load_vectorstore or query
 # load_vectorstore will require all the arguments needed from ´rag_demo\load_chromadb.py´ for the function load_cromadb
-# query will require all the arguments needed from ´rag_demo\retrievals.py´ for the function query_LG_DRYER_DEMO
+# query will require all the arguments needed from ´rag_demo\retrievals.py´ for the function query_LG_DRYER_DEMOimport argparse
+from rag_demo.orchestration import load_chromadb
+from rag_demo.retrievals import query_LG_DRYER_DEMO
+
+def main():
+    parser = argparse.ArgumentParser(description="RAG Demo Orchestration")
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # Subparser for load_vectorstore
+    parser_load = subparsers.add_parser('load_vectorstore', help='Load a document into ChromaDB vector store')
+    parser_load.add_argument('document_name', type=str, help='The name of the document to be loaded')
+    parser_load.add_argument('collection_name', type=str, help='The name of the collection where the document will be stored')
+
+    # Subparser for query
+    parser_query = subparsers.add_parser('query', help='Query the LG_DRYER_DEMO collection from the vectorstore')
+    parser_query.add_argument('prompt', type=str, help='The question to query the vectorstore with')
+
+    args = parser.parse_args()
+
+    if args.command == 'load_vectorstore':
+        result = load_chromadb(args.document_name, args.collection_name)
+        print(result)
+    elif args.command == 'query':
+        result = query_LG_DRYER_DEMO(args.prompt)
+        print(result)
+
+if __name__ == '__main__':
+    main()
