df2bbf0d8dd33690e2378bb1439afc9e6eb5fe1d
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 03:57:18 +0100

    aider: Refactored `utils\classes.py` to include proper documentation and formatting.
    # Aider chat conversation:
    
    ASSISTANT: To properly document and format `utils\classes.py`, we will:
    
    1. Add a module docstring at the top of the file to describe its purpose.
    2. Add a class docstring to `PDFLoader` to explain what the class does.
    3. Add method docstrings to `__init__` and `load_and_split` to describe their functionality.
    4. Ensure proper PEP 8 formatting, such as two blank lines before class definition and one blank line between methods.
    
    Here is the *SEARCH/REPLACE* block for `utils\classes.py`:
    
    utils\classes.py
    ```python
    <<<<<<< SEARCH
    # utils\classes.py
    
    from langchain.document_loaders import PyPDFLoader
    
    class PDFLoader:
    
        def __init__(self, path):
            self.path = path
            self.loader = None
    
        def load_and_split(self):
            self.loader = PyPDFLoader(self.path)
            return self.loader.load_and_split()
    =======
    """
    This module provides a class for loading and processing PDF documents.
    """
    
    from langchain.document_loaders import PyPDFLoader
    
    class PDFLoader:
        """
        A class that wraps the PyPDFLoader to load and split PDF documents.
    
        Attributes:
            path (str): The file path to the PDF document.
            loader (PyPDFLoader): An instance of PyPDFLoader for loading the document.
        """
    
        def __init__(self, path):
            """
            Initializes the PDFLoader with the given file path.
    
            Args:
                path (str): The file path to the PDF document.
            """
            self.path = path
            self.loader = None
    
        def load_and_split(self):
            """
            Loads the PDF document and splits it into pages.
    
            Returns:
                list: A list of pages from the PDF document.
            """
            self.loader = PyPDFLoader(self.path)
            return self.loader.load_and_split()
    >>>>>>> REPLACE
    ```
    
    This block will update the file with appropriate documentation and formatting according to Python's PEP 8 style guide.

--- utils/classes.py
+++ utils/classes.py
@@ -1,13 +1,36 @@
 # utils\classes.py
+"""
+This module provides a class for loading and processing PDF documents.
+"""
 
 from langchain.document_loaders import PyPDFLoader
 
+
 class PDFLoader:
+    """
+    A class that wraps the PyPDFLoader to load and split PDF documents.
+
+    Attributes:
+        path (str): The file path to the PDF document.
+        loader (PyPDFLoader): An instance of PyPDFLoader for loading the document.
+    """
 
     def __init__(self, path):
+        """
+        Initializes the PDFLoader with the given file path.
+
+        Args:
+            path (str): The file path to the PDF document.
+        """
         self.path = path
         self.loader = None
 
     def load_and_split(self):
+        """
+        Loads the PDF document and splits it into pages.
+
+        Returns:
+            list: A list of pages from the PDF document.
+        """
         self.loader = PyPDFLoader(self.path)
         return self.loader.load_and_split()
