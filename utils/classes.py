5bb59b478bc222a0c33590eed7a9237e73e6e8ab
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 03:56:18 +0100

    aider: Added `load_and_split` method to `PDFLoader` class to load and split PDF pages using `PyPDFLoader`.
    # Aider chat conversation:
    
    ASSISTANT: To create the `PDFLoader` class in `utils\classes.py`, we will:
    
    1. Define the class `PDFLoader` with an `__init__` method that takes a file path as a parameter and stores it.
    2. Add a method `load_and_split` to the class that uses `PyPDFLoader` to load and split the PDF pages.
    
    Here is the *SEARCH/REPLACE* block for `utils\classes.py`:
    
    utils\classes.py
    ```python
    <<<<<<< SEARCH
    # I want you to generalize it in a class
    
    class PDFLoader:
    
        def __init__(self, path):
            self.path = path
    =======
    # I want you to generalize it in a class
    
    class PDFLoader:
    
        def __init__(self, path):
            self.path = path
            self.loader = None
    
        def load_and_split(self):
            self.loader = PyPDFLoader(self.path)
            return self.loader.load_and_split()
    >>>>>>> REPLACE
    ```
    
    This code block will add the `load_and_split` method to the `PDFLoader` class, which will create a `PyPDFLoader` instance and return the loaded and split pages when called.

--- utils/classes.py
+++ utils/classes.py
@@ -11,3 +11,8 @@ class PDFLoader:
 
     def __init__(self, path):
         self.path = path
+        self.loader = None
+
+    def load_and_split(self):
+        self.loader = PyPDFLoader(self.path)
+        return self.loader.load_and_split()
