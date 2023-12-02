54a349cf89c534decaabcdc74931e5b25e2d4777
Andrea Stefanachi <andrea.stefanachi@unbelievable-machine.com>
2023-12-02 04:59:27 +0100

    aider: Formatted and commented `prompt_templates.py` and `retrievals.py` according to PEP 8 standards.
    # Aider chat conversation:
    
    ASSISTANT: To properly comment and format `prompt_templates.py` and `retrievals.py`, we will add appropriate comments to explain the purpose of the code and its functions, and ensure that the formatting adheres to PEP 8 standards, which is the style guide for Python code.
    
    Let's start with `rag_demo\prompt_templates.py`:
    
    rag_demo\prompt_templates.py
    ```python
    <<<<<<< SEARCH
    # /llm/prompt_templates.py
    ## Langchain
    from langchain.prompts import load_prompt
    ## Utilities
    from utils.constants import PROMPT_YAML_DIR
    ## Python Core
    import os
    
    # Define here your prompt templates load_prompt(os.path.join(PROMPT_YAML_DIR, "yourprompt.yaml"))
    
    rag_prompt_custom = load_prompt(os.path.join(PROMPT_YAML_DIR, "rag_custom_prompt.yaml"))
    
    hyde_custom_prompt = load_prompt(os.path.join(PROMPT_YAML_DIR, "hyde_custom_prompt.yaml"))
    =======
    # prompt_templates.py
    # This module is responsible for loading custom prompt templates for the RAG demo.
    
    # External imports
    from langchain.prompts import load_prompt  # Langchain library to load prompts
    import os  # Python Core library for OS interface
    
    # Internal imports
    from utils.constants import PROMPT_YAML_DIR  # Utility constants for the project
    
    # Load custom RAG prompt template from YAML file
    rag_prompt_custom = load_prompt(os.path.join(PROMPT_YAML_DIR, "rag_custom_prompt.yaml"))
    
    # Load custom Hyde prompt template from YAML file
    hyde_custom_prompt = load_prompt(os.path.join(PROMPT_YAML_DIR, "hyde_custom_prompt.yaml"))
    >>>>>>> REPLACE
    ```
    
    Now, let's format and comment `rag_demo\retrievals.py`:
    
    rag_demo\retrievals.py
    ```python
    <<<<<<< SEARCH
    # /llm/retrievals.py
    from .prompt_templates import rag_prompt_custom
    from .retrieval_from_chromadb import run_qa_chromadb
    
    def query_LG_DRYER_DEMO(prompt: str):
        '''Returns a context from a question regarding the LG_DRYER_DEMO collection from the vectorstore'''
        return run_qa_chromadb(question=prompt, collection_name="LG_DRYER_DEMO", template=rag_prompt_custom)
    =======
    # retrievals.py
    # This module defines functions to perform retrieval operations for the RAG demo.
    
    # Relative imports from within the rag_demo package
    from .prompt_templates import rag_prompt_custom  # Custom prompt templates for RAG
    from .retrieval_from_chromadb import run_qa_chromadb  # Function to run QA on ChromaDB
    
    def query_LG_DRYER_DEMO(prompt: str) -> str:
        """
        Queries the LG_DRYER_DEMO collection from the vectorstore and returns the context.
    
        Args:
            prompt (str): The question to query the vectorstore with.
    
        Returns:
            str: The retrieved context from the vectorstore.
        """
        return run_qa_chromadb(question=prompt, collection_name="LG_DRYER_DEMO", template=rag_prompt_custom)
    >>>>>>> REPLACE
    ```
    
    These changes add more descriptive comments and ensure that the code is formatted according to PEP 8 standards.

--- rag_demo/prompt_templates.py
+++ rag_demo/prompt_templates.py
@@ -1,13 +1,15 @@
 # /llm/prompt_templates.py
-## Langchain
-from langchain.prompts import load_prompt
-## Utilities
-from utils.constants import PROMPT_YAML_DIR
-## Python Core
-import os
+# This module is responsible for loading custom prompt templates for the RAG demo.
 
-# Define here your prompt templates load_prompt(os.path.join(PROMPT_YAML_DIR, "yourprompt.yaml"))
+# External imports
+from langchain.prompts import load_prompt  # Langchain library to load prompts
+import os  # Python Core library for OS interface
 
+# Internal imports
+from utils.constants import PROMPT_YAML_DIR  # Utility constants for the project
+
+# Load custom RAG prompt template from YAML file
 rag_prompt_custom = load_prompt(os.path.join(PROMPT_YAML_DIR, "rag_custom_prompt.yaml"))
-  
+
+# Load custom Hyde prompt template from YAML file
 hyde_custom_prompt = load_prompt(os.path.join(PROMPT_YAML_DIR, "hyde_custom_prompt.yaml"))
--- rag_demo/retrievals.py
+++ rag_demo/retrievals.py
@@ -1,7 +1,18 @@
 # /llm/retrievals.py
-from .prompt_templates import rag_prompt_custom
-from .retrieval_from_chromadb import run_qa_chromadb
+# This module defines functions to perform retrieval operations for the RAG demo.
 
-def query_LG_DRYER_DEMO(prompt: str):
-    '''Returns a context from a question regarding the LG_DRYER_DEMO collection from the vectorstore'''
+# Relative imports from within the rag_demo package
+from .prompt_templates import rag_prompt_custom  # Custom prompt templates for RAG
+from .retrieval_from_chromadb import run_qa_chromadb  # Function to run QA on ChromaDB
+
+def query_LG_DRYER_DEMO(prompt: str) -> str:
+    """
+    Queries the LG_DRYER_DEMO collection from the vectorstore and returns the context.
+
+    Args:
+        prompt (str): The question to query the vectorstore with.
+
+    Returns:
+        str: The retrieved context from the vectorstore.
+    """
     return run_qa_chromadb(question=prompt, collection_name="LG_DRYER_DEMO", template=rag_prompt_custom)
